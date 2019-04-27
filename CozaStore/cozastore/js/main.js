2ba82aa7dfbd12ea8a9bd1f691666240a2e9e596
Ermiira <Ermiira@users.noreply.github.com>
2019-04-28 00:25:59 +0200

    Pj2

--- CozaStore/cozastore/js/main.js
+++ CozaStore/cozastore/js/main.js
@@ -277,6 +277,23 @@
         $('.js-modal1').removeClass('show-modal1');
     });
 
+    $('.js-show-modal2').on('click',function(e){
+        e.preventDefault();
+        $('.js-modal2').addClass('show-modal1');
+    });
+
+    $('.js-hide-modal2').on('click',function(){
+        $('.js-modal2').removeClass('show-modal1');
+    });
+
+    $('.js-show-modal3').on('click',function(e){
+        e.preventDefault();
+        $('.js-modal3').addClass('show-modal1');
+    });
+
+    $('.js-hide-modal3').on('click',function(){
+        $('.js-modal3').removeClass('show-modal1');
+    });
 
 
 })(jQuery);
