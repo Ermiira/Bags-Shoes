98b73942d013f7bacf3b7a1b89004d753f403911
Ermiira <Ermiira@users.noreply.github.com>
2019-07-03 18:50:22 +0200

    Products for category and subcatgeory

--- CozaStore/product.routes.js
+++ CozaStore/product.routes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const SqlProvider = require('./sql.provider');
+const path=require("path");
+const bodyParser=require('body-parser');
 //const ProductService = require('./product.service');
 const HTTPStatus = require('http-status');
-var bodyParser = require('body-parser');
+const fs=require('fs');   
+const multer = require('multer');  
+const upload = multer({dest: __dirname + '/public/images'},
+                      {filename: function (req, file, cb) {
+                      cb(null, file.originalname + '.jpg')
+      }   
+});
+
 
 router.use(express.static('./views'))
 router.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -12,60 +21,209 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 
 
-router.get('/products',async function (req, res) {
+router.get('/',async function (req, res) {
     
   const connection = await SqlProvider.getConnection();
 
   await connection.query('SELECT * FROM products', function (error, results, fields) {
       if (error) throw error;
-      res.json(results);
+      
+      res.render("product",{ results: results});
     });
-   
+    res.json(results);
 });
 
 
-router.get('/category',async function (req, res) {
+router.get('/bags',async function (req, res) {
     
   const connection = await SqlProvider.getConnection();
 
-  await connection.query('SELECT * FROM `category`', function (error, results, fields) {
+  await connection.query('SELECT * FROM `products` where `categoryId`=1 ', function (error, results, fields) {
       if (error) throw error;
-      res.json(results);
+      res.render("product", { results:results })
     });
-   
+   res.json(results);
 });
+
+router.get('/shoes',async function (req, res) {
+    
+  const connection = await SqlProvider.getConnection();
+
+  await connection.query('SELECT * FROM `products` where `categoryId`=2 ', function (error, results, fields) {
+      if (error) throw error;
+      res.render("product", { results:results })
+    });
+   res.json(results);
+});
+
+
+
+
+
+router.get('/bags/Wallets',async function (req, res) {
+    
+  const connection = await SqlProvider.getConnection();
+
+  await connection.query('SELECT * FROM `products` where subcategoryId=1', function (error, results, fields) {
+      if (error) throw error;
+      res.render("product", { results:results })
+    });
+   res.json(results);
+});
+
+
+
+router.get('/bags/Belt',async function (req, res) {
+    
+  const connection = await SqlProvider.getConnection();
+
+  await connection.query('SELECT * FROM `products` where subcategoryId=2', function (error, results, fields) {
+      if (error) throw error;
+      res.render("product", { results:results })
+    });
+   res.json(results);
+});
+
+
+
+router.get('/bags/Shoulder',async function (req, res) {
+    
+  const connection = await SqlProvider.getConnection();
+
+  await connection.query('SELECT * FROM `products` where subcategoryId=3', function (error, results, fields) {
+      if (error) throw error;
+      res.render("product", { results:results })
+    });
+   res.json(results);
+});
+
+
+
+
+router.get('/bags/Clutch',async function (req, res) {
+    
+  const connection = await SqlProvider.getConnection();
+
+  await connection.query('SELECT * FROM `products` where subcategoryId=4', function (error, results, fields) {
+      if (error) throw error;
+      res.render("product", { results:results })
+    });
+   res.json(results);
+});
+
+
+
+router.get('/bags/Accessories',async function (req, res) {
+    
+  const connection = await SqlProvider.getConnection();
+
+  await connection.query('SELECT * FROM `products` where subcategoryId=5', function (error, results, fields) {
+      if (error) throw error;
+      res.render("product", { results:results })
+    });
+   res.json(results);
+});
+
+
+router.get('/shoes/Sandals',async function (req, res) {
+    
+  const connection = await SqlProvider.getConnection();
+
+  await connection.query('SELECT * FROM `products` where subcategoryId=6', function (error, results, fields) {
+      if (error) throw error;
+      res.render("product", { results:results })
+    });
+   res.json(results);
+});
+
+
+
+router.get('/shoes/Heels',async function (req, res) {
+    
+  const connection = await SqlProvider.getConnection();
+
+  await connection.query('SELECT * FROM `products` where subcategoryId=7', function (error, results, fields) {
+      if (error) throw error;
+      res.render("product", { results:results })
+    });
+   res.json(results);
+});
+
+
+router.get('/shoes/Sneakers',async function (req, res) {
+    
+  const connection = await SqlProvider.getConnection();
+
+  await connection.query('SELECT * FROM `products` where subcategoryId=8', function (error, results, fields) {
+      if (error) throw error;
+      res.render("product", { results:results })
+    });
+   res.json(results);
+});
+
+
+router.get('/shoes/Boots',async function (req, res) {
+    
+  const connection = await SqlProvider.getConnection();
+
+  await connection.query('SELECT * FROM `products` where subcategoryId=9', function (error, results, fields) {
+      if (error) throw error;
+      res.render("product", { results:results })
+    });
+   res.json(results);
+});
+
+
+
+router.get('/shoes/Casual',async function (req, res) {
+    
+  const connection = await SqlProvider.getConnection();
+
+  await connection.query('SELECT * FROM `products` where subcategoryId=10', function (error, results, fields) {
+      if (error) throw error;
+      res.render("product", { results:results })
+    });
+   res.json(results);
+});
+
 
 
 router.get('/subcategory',async function (req, res) {
     
   const connection = await SqlProvider.getConnection();
-
-  await connection.query('SELECT * FROM `subcategory`', function (error, results, fields) {
-      if (error) throw error;
-      res.json(results);
-    });
+  
+  var sqlcs='select c.categoryId,c.catName,s.subcategoryId ,s.subcatName from category c, subcategory s where c.categoryId=s.categoryId';
    
+  await connection.query(sqlcs, function (error, subcategory, fields) {
+    if (error) throw error;
+
+    res.render("index",{subcategory:subcategory}) ;
+  });
+
+   res.json(subcategory);
+
 });
 
-router.get('/produkti/:id', async function (req, res) {
+router.get('/:id', async function (req, res) {
  
     const connection = await SqlProvider.getConnection();
 
     var productId= req.params.id;
     var sql = 'SELECT * FROM `products` where productId=?';
 
-     var result = await connection.query(sql ,[productId])
-
-       res.end(JSON.stringify(result)) ;
+     var results = await connection.query(sql ,[productId])
+     res.render("product-detail", { results:results })
+       //res.json(result);
+      // res.end(JSON.stringify(result)) ;
   
 });
 
 
-router.delete('/products/:id',async function (req, res) {
+router.delete('/:id',async function (req, res) {
 
   const connection = await SqlProvider.getConnection();
 
-  connection.query('DELETE FROM `products` WHERE `productId`=?', [req.params.id], function (error, results) {
+  await connection.query('DELETE FROM `products` WHERE `productId`=?', [req.params.id], function (error, results) {
    if (error) throw error;
    console.log("Deleted!!")
    res.end('Record has been deleted!');
@@ -73,18 +231,64 @@ router.delete('/products/:id',async function (req, res) {
 });
 
 
+router.get("/photo/:id", async function(req,res){
+  const connection = await SqlProvider.getConnection();
+
+  var productId= req.params.id;
+  var sql = 'SELECT photourl FROM `products` where productId=?';
+
+   var result = await connection.query(sql ,[productId])
+   res.render(result);
+
+});
+
+
+router.get("/inserto.html",function(req,res){
+  res.sendFile('inserto.html', {root : __dirname + '/views'});
+});
+
+router.post("/test", upload.single('foto'),async function(req, res) {
+ const connection = await SqlProvider.getConnection();
+
+  const url= req.file.path 
+  const img= fs.readFileSync(req.file.path);
+  const emri = req.body.emri1
+  const cmimi = req.body.cmimi1
+  const pesha = req.body.pesha1
+  const pershkrimi = req.body.pershkrimi1
+  const kategoria = req.body.kategoria1
+  const nenkategoria = req.body.nenkategoria1
+  const photoname=req.file.originalname
+  
+//kjo query ka shku e njejt qysh u kan
+  const sql = "INSERT INTO products (categoryId,subcategoryId,pName, pPrice, pWeight, pDescription,photoblob,photourl,photoname) VALUES (?,?,?,?,?,?,?,?,?)";
+   connection.query(sql,[kategoria,nenkategoria,emri,cmimi,pesha,pershkrimi,img,url,photoname], (err, results, fields) => {
+  if (err) {
+    console.log("Insertimi deshtoi. " + err)
+    res.sendStatus(500)
+    return
+  }
+    console.log("Insertimi u krye me sukses")
+    res.end()
+ });
+  });
+
+
+
+
+//me ndreq, duhet me ndreq qeta
 router.post('/:id/photos', async function (req, res) {
 
-  const url = './public/images/' + req.files.photo.name;
-  req.files.photo.mv(url, async function (error) {
+  const url = './public/images/'+req.params.photos;
+  req.files.foto.mv(url, async function (error) {
       if (error) {
-          return res.send(HTTPStatus.INTERNAL_SERVER_ERROR).end();
+          return res.send(error);
       }
       const connection = await SqlProvider.getConnection();
 
       const result = await connection.query('UPDATE `products` SET ? where productId=?', [
           {
-            photoUrl: url
+            photourl: url
           },
           req.params.id]);
 
@@ -98,46 +302,22 @@ router.post('/:id/photos', async function (req, res) {
   });
 });
 
+router.use(express.static('public'));
 
-router.post("/user_create",async function(req, res){
-
-  const connection = await SqlProvider.getConnection();
-
-  const emri = req.body.emri1
-  const cmimi = req.body.cmimi1
-  const pesha = req.body.pesha1
-  const pershkrimi = req.body.pershkrimi1
-  const kategoria = req.body.kategoria1
-  const nenkategoria = req.body.nenkategoria1
-  const foto = req.body.foto
-
-
-  const sql = "INSERT INTO products (categoryId,subcategoryId,pName, pPrice, pWeight, pDescription,photoUrl) VALUES (?,?,?,?,?,?,?)";
-  await connection.query(sql,[kategoria,nenkategoria,emri,cmimi,pesha,pershkrimi,foto], (err, results, fields) => {
-  if (err) {
-    console.log("Insertimi deshtoi. " + err)
-    res.sendStatus(500)
-    return
-  }
-    console.log("Insertimi u krye me sukses")
-    res.end()
- });
-  });
-
-
-
+//url edhe blob s'ruhet
 router.post('/',  async function (req, res) {
 
     const product = {
-
+//kjo eshte posti qe bahet anej ka postman-i
         categoryId: req.body.categoryId,
         subcategoryId: req.body.subcategoryId,
         pName: req.body.name,
         pPrice: req.body.price,
         pWeight: req.body.weight,
         pDescription:req.body.description,
-       // photoUrl: req.body.Url
-        
+        photoblob: req.body.pblob,
+        photourl: req.body.url
+        //photourl:req.file.path
     }
 
     const connection = await SqlProvider.getConnection();
@@ -150,12 +330,11 @@ router.post('/',  async function (req, res) {
     }
 
     return res.send(HTTPStatus.OK).end();
-}
-);
+});
 
+//tash ta provojm
 
-
-
+//kta se kom prek se ka pas ba
   router.put('/:id', async function (req, res) {
     
     const produkt = {}
@@ -184,7 +363,6 @@ router.post('/',  async function (req, res) {
 
 
 
-  
+
 
  module.exports = router;
-
--- CozaStore/server.js
+++ CozaStore/server.js
@@ -1,10 +1,14 @@
 var express =require('express');
 var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser');
+const SqlProvider = require('./sql.provider');
 var app = express();
 const productRoutes = require('./product.routes');
-
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: false
+  }));
 //mju qas me req.body.(emri te html name=)
-const bodyParser=require('body-parser');
+//const bodyParser=require('body-parser');
 
 
 //set port
@@ -16,11 +20,23 @@ app.set('view engine', 'handlebars');
 
 
 app.use(express.json()); // add HTTP body to req.body
+app.use('/public/', express.static('public')); // sherben files static
 
 
 //routes
-app.get("/",function(req,res){
-    res.render("index");
+app.get("/", async function(req,res){
+    const connection = await SqlProvider.getConnection();
+  
+  var sqlcs='select c.categoryId,c.catName,s.subcategoryId ,s.subcatName from category c, subcategory s where c.categoryId=s.categoryId';
+   
+  await connection.query(sqlcs, function (error, subcategory, fields) {
+    if (error) throw error;
+
+    res.render("index",{subcategory:subcategory}) ;
+  });
+
+   res.json(subcategory);
+   
 })
 
 app.get("/about",function(req,res){
@@ -30,12 +46,8 @@ app.get("/about",function(req,res){
 app.get("/contact",function(req,res){
     res.render("contact");
 })
-
-
-app.get("/product",function(req,res){
-  res.render("product");
- })
-
+    
+  
  app.get("/product-detail",function(req,res){
     res.render("product-detail");
    })
@@ -52,4 +64,3 @@ app.listen(port,function(){
     console.log("app running");
 })
 
-app.use('/public/', express.static('public')); // sherben files static
